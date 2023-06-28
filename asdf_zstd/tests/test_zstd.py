e245cf3fe79c82eb40f55e62de7e9c07708335c4
Martin Spacek <martin.spacek@ipp.mpg.de>
2023-06-28 13:17:29 +0200

    Copy more complicated compression tests from asdf

--- asdf_zstd/tests/test_zstd.py
+++ asdf_zstd/tests/test_zstd.py
@@ -1,11 +1,61 @@
-from asdf.config import config_context
-from asdf.testing.helpers import roundtrip_object
-import numpy
+"""Mostly copied from `asdf/asdf/_tests/test_compression.py`"""
+
+import io
+import os
+
+import numpy as np
+
+import asdf
+from asdf import generic_io
+from asdf._tests import _helpers as helpers
+
+RNG = np.random.default_rng(0) # init random number generator with seed 0
 
 
-def test_zstd():
-    arr = numpy.arange(42)
-    with config_context() as cfg:
-        cfg.all_array_compression = "zstd"
-        arr2 = roundtrip_object({"arr": arr})["arr"]
-        numpy.testing.assert_array_equal(arr2, arr)
+def _get_large_tree():
+    x = RNG.normal(size=(128, 128))
+    return {"science_data": x}
+
+
+def _roundtrip(tmp_path, tree, compression=None, write_options=None, read_options=None):
+    read_options = {} if read_options is None else read_options
+    write_options = {} if write_options is None else write_options.copy()
+    write_options.update(all_array_compression=compression)
+
+    tmpfile = os.path.join(str(tmp_path), "test.asdf")
+
+    ff = asdf.AsdfFile(tree)
+    ff.write_to(tmpfile, **write_options)
+
+    with asdf.open(tmpfile, mode="rw") as ff:
+        ff.update(**write_options)
+
+    with asdf.open(tmpfile, **read_options) as ff:
+        helpers.assert_tree_match(tree, ff.tree)
+
+    # Also test saving to a buffer
+    buff = io.BytesIO()
+
+    ff = asdf.AsdfFile(tree)
+    ff.write_to(buff, **write_options)
+
+    buff.seek(0)
+    with asdf.open(buff, **read_options) as ff:
+        helpers.assert_tree_match(tree, ff.tree)
+
+    # Test saving to a non-seekable buffer
+    buff = io.BytesIO()
+
+    ff = asdf.AsdfFile(tree)
+    ff.write_to(generic_io.OutputStream(buff), **write_options)
+
+    buff.seek(0)
+    with asdf.open(generic_io.InputStream(buff), **read_options) as ff:
+        helpers.assert_tree_match(tree, ff.tree)
+
+    return tmpfile
+
+
+def test_zstd(tmp_path):
+    tree = _get_large_tree()
+    _roundtrip(tmp_path, tree, "zstd")
